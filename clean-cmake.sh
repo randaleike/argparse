79d89900112f607acf790842147a083d672aa5a6
Randal Eike <grocsoftware@gmail.com>
2023-07-15 17:45:32 -0600

    Added debug and release.  Fixed Doxygen generation

--- clean-cmake.sh
+++ clean-cmake.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # check for build
-if [ -d "./build" ] 
+if [ -d "./build-release" ] 
 then 
-    rm -rf ./build
+    rm -rf ./build-release
+fi
+if [ -d "./build-debug" ] 
+then 
+    rm -rf ./build-debug
 fi
--- create_cmake.sh
+++ create_cmake.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # check for build
-if [ ! -d "./build" ] 
+if [ ! -d "./build-release" ] 
 then 
-    mkdir ./build
+    mkdir ./build-release
+fi
+if [ ! -d "./build-debug" ] 
+then 
+    mkdir ./build-debug
 fi
 
-# start the build
-cd ./build
-cmake ..
+
+# Create the make files
+cd ./build-release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cd ..
+
+cd ./build-debug
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 cd ..
